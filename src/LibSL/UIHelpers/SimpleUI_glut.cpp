1dc910c129565e15d726471d9be99aef92c68725
Salim Perchy <yamil-salim.perchy@inria.fr>
2019-02-05 12:46:19 +0100

    linux compilation

--- src/LibSL/UIHelpers/SimpleUI_glut.cpp
+++ src/LibSL/UIHelpers/SimpleUI_glut.cpp
@@ -34,7 +34,11 @@ The fact that you are presently reading this means that you have had
 knowledge of the CeCILL-C license and that you accept its terms.
 -------------------------------------------------------------------- */
 
+#ifdef EMSCRIPTEN
 #include <GL/glut.h>
+#else
+#include <GL/freeglut.h>
+#endif
 
 //---------------------------------------------------------------------------
 // GLUT implementation
