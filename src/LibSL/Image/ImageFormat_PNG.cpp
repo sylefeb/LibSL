f6f6c448b37dd802edf82001900b4b9795271165
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2026-05-19 16:42:07 +0200

    fix:(attempt) emscripten define

--- src/LibSL/Image/ImageFormat_PNG.cpp
+++ src/LibSL/Image/ImageFormat_PNG.cpp
@@ -220,7 +220,7 @@ void NAMESPACE::ImageFormat_PNG::save(const char *fname,const NAMESPACE::Image *
 
 //---------------------------------------------------------------------------
 
-#ifndef EMSCRIPTEN // SL 2025-04-17 hotfix as this result in Emscripten compilation error
+#ifndef __EMSCRIPTEN__ // SL 2025-04-17 hotfix as this result in Emscripten compilation error
 
 template <typename TChar>
 void write_data_stream(png_structp png_ptr, png_bytep data, png_size_t length)
